afb2e87cb555d11d50f2505bf767617be228747b
Ankit Jha <37044020+AnkitJha1998@users.noreply.github.com>
2018-03-27 22:58:42 +0530

    Updated Codes- Check these one out

--- LedWorkingTest/LedWorkingTest.ino
+++ LedWorkingTest/LedWorkingTest.ino
@@ -1,10 +1,14 @@
-
-int pins[5]={2,3,4,5,6};
+int pins[5]={2,3,4,5,7};
 int trigpin=9;
 int echopin=8;
 
 double duration;
 double distance;
+
+boolean access=false;
+String recvData="";
+boolean buzzAlarm=false;
+
 void switchOn(int i)
 {
   for(int i1=0;i1<5;i1++)
@@ -20,32 +24,44 @@ void switchOff()
     digitalWrite(pins[i1],LOW);
   }
 }
+
+
 void setup() {
   Serial.begin(9600);
-  for(int i=0;i<4;i++)pinMode(pins[i],OUTPUT);
+  for(int i=0;i<5;i++)pinMode(pins[i],OUTPUT);
   pinMode(trigpin,OUTPUT);
   pinMode(echopin,INPUT);
 }
-boolean access=false;
+
+
 void loop() {
+  
+  (access=false)?Serial.print("0;\n"):Serial.print("1;\n");
   digitalWrite(trigpin,LOW);
   for(int i=0;i<5;i++)
   {
     digitalWrite(pins[i],LOW);
-  }/*
-  for(int i=0;i<4;i++)
+  }
+  if(Serial.available())
   {
-    digitalWrite(pins[i],HIGH);
-    delay(500);
-    digitalWrite(pins[i],LOW);
-  }*/
+    recvData=Serial.readStringUntil('\n');
+  }
+  if(recvData[0]=='1')
+  {
+    access=true;
+  }
+  if(recvData[0]=='2')
+  {
+    access=false;
+    
+  }
+  
   digitalWrite(trigpin,HIGH);
   delayMicroseconds(10);   //Creating 10 second microPulse
   digitalWrite(trigpin,LOW);
   duration=pulseIn(echopin,HIGH);
   distance=duration*0.034/2;
-  Serial.print(distance);
-  Serial.println(" cm");
+  
   if(access==true)
   {
       if(distance<20&&distance>11){switchOn(2);}
--- PythonClient.py
+++ PythonClient.py
@@ -1,5 +1,8 @@
-import Serial
+import serial
 import _thread
+import time
+
+ard1=serial.Serial('COM8',9600)
 
 #----------------------------------------------------Case 1---------------------------------------------------
 
@@ -12,32 +15,69 @@ def choose1():
     file = open('dataset.txt','r')
     x = file.readlines()
     x = [j.rstrip('\n') for j in x]
-
+    counter=False
     for i in range(1,4):
         a = input("Enter your Security number : ")
         if check(x,a) == True:
             print("Welcome ", a)
+            counter=True
             break
         elif i<3:
             print("Incorrect Detail. You have ", (3-i), " chances")
         if i>=3:
-            print("Intruder alert")
+            print("! ! ! Intruder suspected ! ! !")
+    if counter==true :
+        str=ard1.readLine()
+        if str[0]=='0':
+            print('! ! ! Intruder Alert ! ! !')
+            while(str!='1'):
+                str=ard.readLine()
+                if str[0]=='1':
+                   break;
+        strWrite="1;\n"
+        ard.write(strWrite.encode("utf-8"))
+        print("\nAccess Granted for thirty seconds.\n\n")
+        time.sleep(30)
+        strWrite="2;\n"
+        while(1):
+            ard.write(strWrite.encode("utf-8"))
+            str11=ard.readLine()
+            if (str11[0]=='0'):
+                break;
+    
     return
 
 #-------------------------------------------------------------------------------------------------------------
-intruder=false;
-def choose2():
-    ard1=serial.Serial('COM8',9600)
-    str=ard.readLine()
-    if(str[0]=='1'):
-        print("! ! ! Intruder Alert ! ! !")
-        intruder=true;
-    
 
+#-------------------------------------------------Case 2-----------------------------------------------------
+
+intruder=False
+def choose2():
+    
+    str=ard.readLine()
+    if str[0]=='1' :
+        print("! ! ! Intruder ALERT ! ! !")
+        intruder=True
+        while(intruder!=False):
+            str=ard.readLine()
+            if str[0]=='0':
+                intruder=False
+                break;
+    return
+    
+#---------------------------------------------------------------------------------------------------------------
+
+
+#----------------------------------------------Case 3------------------------------------------------------------
+def choose3():
+    print("Yet to be figured out")    
+
+
+#----------------------------------------------------------------------------------------------------------------
 
 choose=1
 while(1):
-    choose=int(input("\n1. Enter ID\n2. Check for Intruder alarm\n3. Face Recognition\n\4. Turn off Alarm(If Intruder Detected)n5. Exit\n\nChoose:"))
+    choose=int(input("\n1. Enter ID\n2. Check for Intruder alarm\n3. Face Recognition\n4. Exit\n\nChoose:"))
     if choose==1:
         choose1()
     elif choose==2:
@@ -45,7 +85,5 @@ while(1):
     elif choose==3:
         choose3()
     elif choose==4:
-        choose4()
-    elif choose==5:
         break;
 print("Bye")
